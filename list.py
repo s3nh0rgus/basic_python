424a608c2c2177f8530ed63e61cfb305d69959eb
Gustavo <mr.rittaline@gmail.com>
2022-12-22 06:57:06 -0300

    Starting with list

---
+++ list.py
@@ -0,0 +1,11 @@
+#list is a items collection in a particular order
+
+names = ["Gustavo", "Claudio", "Iolanda", "Zilma", "Tiago"]
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(names[0])
+print(numbers[9])
+print(names[3].upper())
+print(numbers[-1], numbers[-2], numbers[-3])
+
+print("My friend is called " + names[4])
+ 
