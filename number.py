26bdfcc1d2d37564d01062f61ae4c7aa7a1de2d0
Gustavo <mr.rittaline@gmail.com>
2022-12-21 22:06:16 -0300

    Starting with numbers

---
+++ number.py
@@ -0,0 +1,22 @@
+# arithmetic operations
+
+addition = 1 + 1
+substraction = 9 - 4
+multiplication = 25 * 5
+division = 15 / 3
+exponentation = 4 ** 2
+print(exponentation, division, multiplication, substraction, addition)
+
+arithmetic_expression = 20 * 2 + 2
+print(arithmetic_expression)
+arithmetic_expression = 20 * (2 + 2)
+print(arithmetic_expression)
+
+#float
+
+addition = 0.3 + 0.5
+print(addition)
+problem_float = 0.2 + 0.1
+print(problem_float)
+problem_float = 3 * 0.1
+print(problem_float)
