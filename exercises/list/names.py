dcce7c6355881ce08874ae4c5b2f2d1d6be48bf9
Gustavo <mr.rittaline@gmail.com>
2022-12-22 06:57:30 -0300

    List exercises

---
+++ exercises/list/names.py
@@ -0,0 +1,5 @@
+names = ["Vitor", "Tiago", "Luiz", "Brenno"]
+print("This is my friend " + names[0])
+print("This is my friend " + names[1])
+print("This is my friend " + names[2])
+print("This is my friend " + names[3])
---
+++ exercises/list/vehicles.py
@@ -0,0 +1,5 @@
+cars = ["Range Rover", "Mercedes", "BMW X1", "Porshe Panamera"]
+print("I would like to drive a " + cars[0])
+print("I have a " + cars[1])
+print("The best car is " + cars[2])
+print("I look prettier than " + cars[3])
