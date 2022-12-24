e0e6a572ebfec4b3f710944d44480053bb5706ac
Gustavo <mr.rittaline@gmail.com>
2022-12-23 21:20:40 -0300

    Playing with lists

--- list.py
+++ list.py
@@ -8,4 +8,34 @@ print(names[3].upper())
 print(numbers[-1], numbers[-2], numbers[-3])
 
 print("My friend is called " + names[4])
- 
+
+# names[3] = "Vitor"
+names.append("Vitor")
+print(names)
+
+superheros = []
+superheros.append("Superman")
+superheros.append("Iron Man")
+superheros.append("Batman")
+superheros.append("Hulk")
+superheros.insert(1, "Wonder Woman")
+superheros.insert(2, "Black Panther")
+print(superheros)
+
+del superheros[5]
+print(superheros)
+
+popped_name = names.pop()
+print(names)
+print(popped_name)
+my_name = names.pop(0)
+print(names)
+print("My names is " + my_name)
+
+countrys = ["Brazil", "United States", "England", "Japan", "South Korean"]
+print(countrys)
+imperialist_country = "United States"
+# countrys.remove("United States")
+countrys.remove(imperialist_country)
+print(countrys)
+
