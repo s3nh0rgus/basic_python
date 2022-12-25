41d1e462a43e7d1449e94d0c7930789576e380b8
Gustavo <mr.rittaline@gmail.com>
2022-12-25 08:43:33 -0300

    Starting with for

---
+++ for.py
@@ -0,0 +1,6 @@
+programmers = ["linus torvalds", "richard stallman", "john carmark", "guido van rossum"]
+
+for programmer in programmers:
+    print(programmer.title() + " was part of computer history")
+
+print("Thanks for good work") 
