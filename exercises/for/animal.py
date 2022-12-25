2eed9e89f5b0b60d28903223b9050be318e28362
Gustavo <mr.rittaline@gmail.com>
2022-12-25 08:44:06 -0300

    For exercises

---
+++ exercises/for/animal.py
@@ -0,0 +1,6 @@
+animals = ["cat", "tiger", "lion", "panther"]
+
+for animal in animals:
+    print("Beatiful like a " + animal)
+    
+print("Felines are very beatiful! <3")
---
+++ exercises/for/pizza.py
@@ -0,0 +1,6 @@
+pizzas = ["portuguesa", "calabresa", "pepperoni", "quatro queijos"]
+
+for pizza in pizzas:
+    print("Pizza de " + pizza + " é a minha favorita!")
+    
+print("Pizza é a minha comida favorita")
