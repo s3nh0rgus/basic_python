b6e9dc9f09fdb2c2e9262ef77638550a73bc4dcc
Gustavo <mr.rittaline@gmail.com>
2022-12-28 05:46:31 -0300

    Adapting the code to PEP-8

--- for.py
+++ for.py
@@ -1,4 +1,5 @@
-programmers = ["linus torvalds", "richard stallman", "john carmark", "guido van rossum"]
+programmers = ["linus torvalds", "richard stallman", "john carmark", 
+               "guido van rossum"]
 
 for programmer in programmers:
     print(programmer.title() + " was part of computer history")
@@ -8,8 +9,9 @@ print("Thanks for good work")
 # function range(start;end)
 for i in range(1,6): 
     print(i)
-# DISCLAIMER: when i arrive in 6, the loop stop and do not show 6. This is like navigation in a list: [0, 1, 2, 3...], but in this case the problem of 1 is
-# +1 while in list is -1. 
+# DISCLAIMER: when i arrive in 6, the loop stop and do not show 6. This is 
+# like navigation in a list: [0, 1, 2, 3...], but in this case the problem 
+# of 1 is +1 while in list is -1. 
 
 numbers = list(range(1,6))
 print(numbers)
--- slice.py
+++ slice.py
@@ -22,5 +22,8 @@ print("\nFoods:")
 print(foods)
 print("My favorite foods:")
 print(favorite_foods)
-# Why isn't this (foods = favorite_foods) method copying one list to another? Because, when you use this method just is coupling other variable to one list,
-# that already is coupling with one variable, then these two variable are sharing the same list. If you want a new list copy of another list, use slices!
+# Why isn't this (foods = favorite_foods) method copying one list to another? 
+# Because, when you use this method just is coupling other variable to one 
+# list, that already is coupling with one variable, then these two variable 
+# are sharing the same list. If you want a new list copy of another list, use 
+# slices!
