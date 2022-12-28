fab68fa54fb57345333a309eb0970380ac67bddf
Gustavo <mr.rittaline@gmail.com>
2022-12-27 21:11:00 -0300

    Tupla exercises

---
+++ exercises/tupla/buffet.py
@@ -0,0 +1,10 @@
+menu = ("chicken fillet", "steak with onions", "omelet", "spaghetti", "shrimp")
+print("Menu:")
+for dish in menu:
+    print(dish.title())
+
+# menu[4] = "ice cream"
+menu = ("chicken fillet", "strawberry cake", "omelet", "spaghetti", "ice cream")
+print("\nNew menu:")
+for dish in menu:
+    print(dish.title())
