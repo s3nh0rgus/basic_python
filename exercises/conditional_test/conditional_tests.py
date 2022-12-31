f0770fed825e5a7d205c799c3d74835eda6f9dfa
Gustavo <mr.rittaline@gmail.com>
2022-12-31 10:29:40 -0300

    Conditional exercises

---
+++ exercises/conditional_test/conditional_tests.py
@@ -0,0 +1,24 @@
+word = "robot"
+print("Is word == 'robot'?")
+print(word == 'robot')
+print("Is word != 'food'?")
+print(word != 'food')
+
+serie = "Game of Thrones"
+print("Is serie == 'game of thrones'?")
+print(serie == 'game of thrones')
+print(serie.lower() == 'game of thrones')
+
+print("\n9 != 9: " + str(9 != 9))
+print("(1 + 1) == 2: " + str((1 + 1) == 2))
+print("(8 * 8) > 60: " + str((8 * 8) > 60))
+print("(4 * 4) >= 18: " + str((4 * 4) >= 18))
+print("(1 * 100 - 53) < 50: " + str((1 * 100 - 53) < 50))
+print("(2022 - 1971) <= 51: " + str((2022 - 1971) <= 51))
+
+print("\n('Gustavo' == 'Gustavo') and ('Gustavo' != 'gustavo'): " + str(("Gustavo" == "Gustavo") and ("Gustavo" != "gustavo")))
+print("(1 + 1) == 2 or (1 + 1) == 3: " + str((1 + 1) == 2 or (1 + 1) == 3))
+
+books = ["Fight Club", "The Demons", "Fathers and Sons"]
+print("\n'Fathers and Sons' in books: " + str('Fathers and Sons' in books))
+print("'The Trial' in books: " + str("The Trial" in books))
---
+++ exercises/conditional_test/true_or_false.py
@@ -0,0 +1,30 @@
+
+car = 'toyota'
+print("Is car == 'toyota'? I predict True.")
+print(car == 'toyota')
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
+
+animal = 'penguim'
+print("\nIs animal == 'penguim'? I predict True.")
+print(animal == 'penguim')
+print("Is animal == 'tiger'? I predict False.")
+print(animal == 'tiger')
+
+candy = 'bullet'
+print("\nIs candy == 'lollipop'? I predict False.")
+print(candy == 'lollipop')
+print("Is candy == 'bullet'? I predict True.")
+print(candy == 'bullet')
+
+season = 'spring'
+print("\nIs season == 'winter'? I predict False.")
+print(season == 'winter')
+print("Is season == 'spring'? I predict True.")
+print(season == 'spring')
+
+spice = 'sugar'
+print("\nIs spice == 'suagr'? I predict True.")
+print(spice == 'sugar')
+print("Is spice == 'salt'? I predict False.")
+print(spice == 'salt')
